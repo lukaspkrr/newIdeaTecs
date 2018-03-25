e8f704070e5eb680d30169ebbd169a6397502f3c
Lucas Marques Franco <lucas10.mf@gmail.com>
2018-03-25 17:15:36 -0300

    titulos

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,3 +1,4 @@
+import { ServicosService } from './../servicos.service';
 import { Component, OnInit } from '@angular/core';
 
 import { TransitionController, Transition, TransitionDirection } from 'ng2-semantic-ui';
@@ -11,9 +12,11 @@ export class InicioComponent implements OnInit {
   public transitionController = new TransitionController();
   transitionName:string = "fly left"
 
-  constructor() { }
+  constructor(private servicosService :ServicosService) { }
 
   ngOnInit() {
+    this.servicosService.setTitulo('Avaliação da satisfação dos usuários com o serviço de saúde');
+    this.servicosService.setSubtitulo('Quality Saúde');
       this.transitionController.animate(
           new Transition(this.transitionName, 500, TransitionDirection.In, () => console.log("Completed transition.")));
   }
--- src/app/inst-saude/inst-saude.component.ts
+++ src/app/inst-saude/inst-saude.component.ts
@@ -1,5 +1,7 @@
-import { TransitionController, Transition, TransitionDirection } from 'ng2-semantic-ui';
 import { Component, OnInit } from '@angular/core';
+
+import { ServicosService } from './../servicos.service';
+import { TransitionController, Transition, TransitionDirection } from 'ng2-semantic-ui';
 
 @Component({
   selector: 'app-inst-saude',
@@ -10,9 +12,11 @@ export class InstSaudeComponent implements OnInit {
   public transitionController = new TransitionController();
   transitionName:string = "fly left";
 
-  constructor() { }
+  constructor(private servicosService :ServicosService) { }
 
   ngOnInit() {
+    this.servicosService.setTitulo('Instituição de saúde');
+    this.servicosService.setSubtitulo('');
       this.transitionController.animate(
           new Transition(this.transitionName, 500, TransitionDirection.In, () => console.log("Completed transition.")));
   }
